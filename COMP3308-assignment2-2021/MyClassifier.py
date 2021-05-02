044bea3a4fc2554d1e8ca431d1d420f20354edac
Guangyi Liu <gliu9705@uni.sydney.edu.au>
2021-05-02 15:54:40 +0800

    ass eucliden_distance

--- COMP3308-assignment2-2021/MyClassifier.py
+++ COMP3308-assignment2-2021/MyClassifier.py
@@ -1,17 +1,27 @@
-import numpy
+import numpy as np
 import os
 import csv
 
-cwd = os.getcwd()
-
-with open(cwd + "/data/pima-indians-diabetes.csv") as f:
-    reader = csv.reader(f)
-    rows = [row for row in reader]
 
 
+def read_file():
+    cwd = os.getcwd()
+    with open(cwd + "/data/pima-indians-diabetes.csv") as f:
+        reader = csv.reader(f)
+        rows = [row for row in reader]
+    
+    return rows
 
 
+def Euclidean_distance(train_data,test_data):
+	sum = 0
+	for i in range(0,len(test_data)):
+		sum = sum + np.power(train_data[i]-test_data[i],2)
 
+	return np.sqrt(sum)
+
+if __name__ == "__main__":
+    read_file()
 
 
 
