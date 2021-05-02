8c4609734fbcb9e3725c92692c84fb2fb7e12703
Guangyi Liu <gliu9705@uni.sydney.edu.au>
2021-05-02 17:46:29 +0800

    add split dataset full verson

--- COMP3308-assignment2-2021/MyClassifier.py
+++ COMP3308-assignment2-2021/MyClassifier.py
@@ -9,17 +9,31 @@ def read_file():
     cwd = os.getcwd()
     with open(cwd + "/data/pima-indians-diabetes.csv") as f:
         reader = csv.reader(f)
-        rows = [row for row in reader]
+        data = [row for row in reader]
     
-    return rows
+    return data
 
-#step: how many folds in total
-def train_test_split(step):
+#fold: how many folds in total
+def train_test_split(data,fold):
     seed(20210501)
     num = [i for i in range(768)]
     shuffle(num)
-    b = [num[i:i+step] for i in range(0,len(num),step)]
-
+    dataset_split_index = [num[i:i+fold] for i in range(0,len(num),fold)]
+    
+    dataset_split_data = []
+    set_data = []
+    for set_num in dataset_split_index:
+        for j in set_num:
+            set_data.append(data[j])
+        dataset_split_data.append(set_data)
+        set_data = []
+    test = []
+    train = []
+    test = dataset_split_data[0]
+    for _data in dataset_split_data:
+        train.append(_data)
+    return train, test
+    
 
 
 
@@ -32,7 +46,8 @@ def Euclidean_distance(train_data,test_data):
 	return np.sqrt(sum)
 
 if __name__ == "__main__":
-    read_file()
+    data = read_file()
+    
 
 
 
