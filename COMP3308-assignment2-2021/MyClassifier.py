23e001d4b68f0ea5d7ca87f6618e3f5ca19cb376
alfo <61564735+AlwaysLearningFromOthers@users.noreply.github.com>
2021-05-02 13:16:43 +0800

    Add files via upload

---
+++ COMP3308-assignment2-2021/MyClassifier.py
@@ -0,0 +1,18 @@
+import numpy
+import csv
+
+with open("/Users/liuguangyi/Downloads/COMP3308-assignment2-2021/data/pima-indians-diabetes.csv") as f:
+    reader = csv.reader(f)
+    rows = [row for row in reader]
+# print(rows)
+
+
+
+
+
+
+
+
+
+
+
